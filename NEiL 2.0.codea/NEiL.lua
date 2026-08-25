cfcc8624f9cce667713b96c73b0aa848120ea46b
Darth Vader <Chris.Geese@iCloud.com>
2026-08-25 12:20:38 -0400

    added image for neil

--- NEiL 2.0.codea/NEiL.lua
+++ NEiL 2.0.codea/NEiL.lua
@@ -7,12 +7,15 @@ function NEiL:init(pos)
     self.pos = pos or vec2(0, 0)
     self.velocity = vec2(0, 0)
     self.radius = 6
+    self.size = 24
     self.active = false
 end
 
 function NEiL:update(dt)
     if not self.active then return end
+    
     self.pos = self.pos + self.velocity * dt
+    
     if self.pos.x < -self.radius or
     self.pos.x > worldWidth + self.radius or
     self.pos.y < -self.radius or
@@ -23,7 +26,11 @@ end
 
 function NEiL:draw()
     if not self.active then return end
-    fill(theme.fg)
-    noStroke()
-    ellipse(self.pos.x, self.pos.y, self.radius * 2, self.radius * 2)
+    
+    sprite(
+    asset.floating_neil,
+    self.pos.x,
+    self.pos.y,
+    self.size
+    )
 end
