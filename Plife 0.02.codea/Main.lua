ab6f02f282c544af22df3ec78cf4e29810b6ddea
Darth Vader <Chris.Geese@iCloud.com>
2026-09-07 12:27:55 -0400

    project reset

--- Plife 0.02.codea/Main.lua
+++ Plife 0.02.codea/Main.lua
@@ -1,21 +1,14 @@
--- particle life 0.02
+-- 
 -- chris geese @ 2026
 
 function setup()
-    world = World()
-    cam = Camera(world)
 end
 
 function update(dt)
-    world:update(dt)
 end
 
 function draw()
-    cam:apply()
-    world:draw()
-    cam:remove()
 end
 
 function touched(touch)
-    world:touched(touch)
 end
